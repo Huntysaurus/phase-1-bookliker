50afb9e23d60d014db3c0e3b483cf5942127480d
Jacob <yawjacob@yahoo.com>
2021-11-19 16:04:41 -0600

    commit tree d15d496fa631dc571c3de731f7b205527552db34

--- js/index.js
+++ js/index.js
@@ -1,1 +1,98 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const baseURL = "http://localhost:3000/books"
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const list = document.querySelector('#list')
+    const show = document.querySelector('#show-panel')
+
+    function showBook(e){
+        getBook(e).then(book => {
+            const container = `<div>
+                <img src="${book.img_url}">
+                <h1>${book.title}</h1>
+                <h2>${book.author}</h2>
+                <p>${book.description}</p>
+                <ul>
+                ${book.users.map(user => `<li>${user.username}<li/>`).join('')}
+                </ul>
+            </div>`
+            const button = document.createElement('button')
+            // button.innerText == 'LIKE' ? button.innerText = 'UNLIKE' : button.
+            // innerText = 'LIKE'
+            // console.log(button.innerText)
+            // if(button.innerText == "LIKE"){
+            //     console.log('in unlike')
+            //     button.innerText = 'UNLIKE'
+            // } else {
+            //     button.innerText = 'LIKE'
+            // }
+            button.innerText = 'LIKE'
+            if (book.users[book.users.length -1].id == 1){
+                button.innerText = 'UNLIKE'
+            }
+            console.log(button.innerText)
+            button.dataset.bookId = book.id
+            show.innerHTML = container
+            show.appendChild(button)
+        })
+    }
+
+    function getBook(e){
+        return fetch(baseURL + `/${e.target.dataset.bookId}`)
+            .then(res => res.json())
+    }
+
+    function listBooks(books){
+        books.forEach(book => {
+            const item = document.createElement('li')
+            item.dataset.bookId = book.id
+            item.textContent = book.title
+            item.addEventListener('click', showBook)
+            list.appendChild(item)
+        })
+    }
+
+    function getBooks(){
+        fetch(baseURL)
+            .then(res => res.json())
+            .then(books => listBooks(books))
+    }
+
+    function changeLikes(e,id, body){
+        fetch(baseURL + `/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+            .then(() => showBook(e))
+    }
+
+    getBooks()
+
+    function handleClick(e){
+        if(e.target.tagName=="BUTTON"){
+            console.log(e.target)
+            const id = e.target.dataset.bookId
+            if(e.target.innerText == 'LIKE'){
+                getBook(e).then(book => {
+                    const users = book.users
+                    const body = {users: [...users, { "id": 1, "username": "pouros" }]}
+                    changeLikes(e,id, body)
+                })
+            } else {
+                console.log('unlike the book')
+                getBook(e).then(book => {
+                    book.users.pop()
+                    const body = {users: [...book.users]}
+                    changeLikes(e, id, body)
+                })
+            }
+        }
+    }
+
+    show.addEventListener('click', handleClick)
+    
+});
